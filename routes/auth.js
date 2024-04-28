b22846cde0ff62bb5b485a8257c01830abae46a9
anuchit <anuchit.nu567@gmail.com>
2024-04-29 01:42:47 +0700

    register page

--- routes/auth.js
+++ routes/auth.js
@@ -144,6 +144,41 @@ router.post(
   })
 );
 
+router.post("/signup", function (req, res, next) {
+  var salt = crypto.randomBytes(16);
+  crypto.pbkdf2(
+    req.body.password,
+    salt,
+    310000,
+    32,
+    "sha256",
+    function (err, hashedPassword) {
+      if (err) {
+        return next(err);
+      }
+      db.run(
+        "INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)",
+        [req.body.username, hashedPassword, salt],
+        function (err) {
+          if (err) {
+            return next(err);
+          }
+          var user = {
+            id: this.lastID,
+            username: req.body.username,
+          };
+          req.login(user, function (err) {
+            if (err) {
+              return next(err);
+            }
+            res.redirect("/");
+          });
+        }
+      );
+    }
+  );
+});
+
 router.get("/login/federated/facebook", passport.authenticate("facebook"));
 
 router.post("/logout", function (req, res, next) {
