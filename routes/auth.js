40b8b0d8dba96937b98374f6722c9d138a05f1c8
anuchit <anuchit.nu567@gmail.com>
2024-04-29 01:39:55 +0700

    login username passowrd

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,49 @@
 const express = require("express");
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oidc");
+var LocalStrategy = require("passport-local");
+var crypto = require("crypto");
 
 var db = require("../db");
 const router = express.Router();
 require("dotenv").config();
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    db.get(
+      "SELECT * FROM users WHERE username = ?",
+      [username],
+      function (err, row) {
+        if (err) {
+          return cb(err);
+        }
+        if (!row) {
+          return cb(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
 
+        crypto.pbkdf2(
+          password,
+          row.salt,
+          310000,
+          32,
+          "sha256",
+          function (err, hashedPassword) {
+            if (err) {
+              return cb(err);
+            }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+              return cb(null, false, {
+                message: "Incorrect username or password.",
+              });
+            }
+            return cb(null, row);
+          }
+        );
+      }
+    );
+  })
+);
 passport.use(
   new GoogleStrategy(
     {
@@ -93,6 +131,14 @@ router.get("/login/federated/google", passport.authenticate("google"));
 router.get(
   "/oauth2/redirect/google",
   passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+router.post(
+  "/login/password",
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
   })
